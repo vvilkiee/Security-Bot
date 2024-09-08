56a5d2a839d3139235859e99ef7951a42e358bd1
vvilkyraa <dddedxdxd@gmail.com>
2024-09-08 16:33:50 +0200

    Delete invalidRole.js

--- invalidRole.js
+++
@@ -1,9 +0,0 @@
-module.exports = (client) => {
-  client.on('invalidRole', async (message, msg) => {
-    await msg?.delete();
-    let description = `You didn't provide a valid role`;
-    emb = new client.emb().desc(`${client.emoji.cross} ${message.author}: ${description}`);
-
-    return message.reply({ embeds: [emb] });
-  });
-};
