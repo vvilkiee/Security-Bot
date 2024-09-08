dfb167e72be9649f8e9fde2690fcd2a5eac53f86
vvilkyraa <dddedxdxd@gmail.com>
2024-09-08 16:32:25 +0200

    Delete events directory

--- events/custom/invalidChannel.js
+++
@@ -1,9 +0,0 @@
-module.exports = (client) => {
-  client.on('invalidChannel', async (message, msg) => {
-    await msg?.delete();
-    let description = `You didn't provide a valid channel`;
-    emb = new client.emb().desc(`${client.emoji.cross} ${message.author}: ${description}`);
-
-    return message.reply({ embeds: [emb] });
-  });
-};
--- events/custom/invalidUser.js
+++
@@ -1,9 +0,0 @@
-module.exports = (client) => {
-  client.on('invalidUser', async (message, msg) => {
-    await msg?.delete();
-    let description = `You didn't provide a valid user`;
-    emb = new client.emb().desc(`${client.emoji.cross} ${message.author}: ${description}`);
-
-    return message.reply({ embeds: [emb] });
-  });
-};
